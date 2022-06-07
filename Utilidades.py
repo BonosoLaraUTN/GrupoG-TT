4d7f9daa9d4a850ff82a4050473ede0872df91cb
Eduardo Martinez <eduardod.martinez@alumnos.frm.utn.edu.ar>
2022-06-07 13:23:26 -0300

    temporizador y utilidades

---
+++ Utilidades.py
@@ -0,0 +1,17 @@
+import pygame
+
+from Temporizador import *
+
+# cargar sprites (version funky )
+#sprites = pygame.transform.scale2x(pygame.image.load("imagenes/sprites.gif"))
+# cargar sprites (version pixely )
+sprites = pygame.transform.scale(pygame.image.load("imagenes/sprites.gif"), [192, 224])
+#pantalla.set_colorkey((0,138,104))
+
+jugadores = []
+balas = []
+
+reproducir_sonidos = True
+sonidos = {}
+
+gtemporizador = Temporizador()
